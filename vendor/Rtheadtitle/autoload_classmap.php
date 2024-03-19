41cf0a5b4c92f7e0524b930c75bc344b1ceb61db
nhantd292 <86462622+nhantd292@users.noreply.github.com>
2024-03-19 14:12:13 +0700

    remove cache git

--- vendor/Rtheadtitle/autoload_classmap.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @author Remi THOMAS 
- */
-return array(
-    'RtHeadtitle\Module'                                    => __DIR__ . '/Module.php',
-    'RtHeadtitle\Controller\Plugin\HeadTitle'               => __DIR__ . '/src/RtHeadtitle/Controller/Plugin/HeadTitle.php',
-);
